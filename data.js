5ceeaf415d6b29b997d90ac70aa2f7809243549b
Rafael Pufal <30834253+rpufal@users.noreply.github.com>
2021-09-08 20:31:15 -0300

    Update data.js

--- data.js
+++ data.js
@@ -1,10 +1,17 @@
 module.exports = {
+  theMightyHofDragon: {attack: 90, hp: 100},
   fightersStats: {
+    Rogerinho: { attack: 99, hp: 15},
     Mubra: { attack: 50, hp: 400 },
+    Magalu: { attack: 42, hp: 183},
     Surida: { attack: 195, hp: 120 },
     Nyka: { attack: 70, hp: 70 },
+    Shera: {attack:404, hp: 120},
     Nailuc: { attack: 90, hp: 200 },
+    Dollynho: {attack: 1, hp:1},
     Kaimao: { attack: 298, hp: 60 },
-    Nagamor: { attack: 85, hp: 50 }
+    SnoopDog: {attack: 420, hp:247},
+    Nagamor: { attack: 85, hp: 50 },
+    Togepi: { attack: 9, hp: 101}
   }
 }
