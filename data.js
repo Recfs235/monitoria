a920d48443508ee9541873a8e76e76599b1bee18
Joao Nasc <oi@joaonasc.dev>
2021-09-08 20:14:03 -0300

    feat: fighters stats

---
+++ data.js
@@ -0,0 +1,10 @@
+module.exports = {
+  fightersStats: {
+    Mubra: { attack: 50, hp: 400 },
+    Surida: { attack: 195, hp: 120 },
+    Nyka: { attack: 70, hp: 70 },
+    Nailuc: { attack: 90, hp: 200 },
+    Kaimao: { attack: 298, hp: 60 },
+    Nagamor: { attack: 85, hp: 50 }
+  }
+}
